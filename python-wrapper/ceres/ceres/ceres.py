63668829231fb954631be6b4abe41452529ac164
Will Roberts <willprobertss@gmail.com>
2021-02-15 09:54:03 +0000

    Starting to create the argparse handling

--- python-wrapper/ceres/ceres/ceres.py
+++ python-wrapper/ceres/ceres/ceres.py
@@ -1,8 +1,31 @@
+import argparse
+import ceres_init
+
 class Ceres:
     def __init__(self):
         pass
 
+    def handle_args(self):
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers(dest="choice")
+
+        parse_init = subparsers.add_parser("init", help="Initialises Ceres for use on this machine")
+        parse_backup = subparsers.add_parser("backup", help="backup")
+
+        args = parser.parse_args()
+
+        if(args.choice == "init"):
+            ceres_init.init()
+
+        if(args.choice == "backup"):
+            print("Running backup")
+
+        if(args.choice == "run"):
+            print("")
+            # add detached mode
+            # add openvpn flag
     
 
 
 ceres = Ceres()
+ceres.handle_args()
