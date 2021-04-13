6ff00a9e7f52a743386f54c7c1e19e4045e06f20
Will Roberts <willprobertss@gmail.com>
2021-04-13 23:57:43 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Install Ceres
+git clone https://github.com/willrushi/ceres.git
+cd ceres
+pip3 install -r requirements.txt
+sudo python3 setup.py install
